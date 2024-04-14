7a6b1ec4a7d9f0a45c0197150f3bbeef4c5f7b7e
yjy0417 <980420445@qq.com>
2024-04-14 12:56:49 +0800

    s

---
+++ model.py
@@ -0,0 +1,173 @@
+from tensorflow import keras
+import tensorflow as tf
+
+
+def dice_loss(y_true, y_pred, smooth=1e-6):
+    y_true_f = tf.reshape(tf.cast(y_true, tf.float32), [-1])
+    y_pred_f = tf.reshape(tf.cast(y_pred, tf.float32), [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
+    return 1 - score
+
+def bce_dice_loss(y_true, y_pred):
+    bce = tf.keras.losses.binary_crossentropy(y_true, y_pred, label_smoothing=1e-2)
+    dice = dice_loss(y_true, y_pred)
+    return bce + dice
+
+def iou_coefficient(y_true, y_pred, smooth=1e-6):
+    y_true_f = tf.cast(tf.reshape(y_true, [-1]), tf.float32)
+    y_pred_f = tf.cast(tf.reshape(y_pred, [-1]), tf.float32)
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    union = tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) - intersection
+    iou = (intersection + smooth) / (union + smooth)
+    return iou
+
+
+def r_squared(y_true, y_pred):
+    """
+    计算决定系数R^2，用作Keras模型的评估指标。
+
+    :param y_true: 真实值
+    :param y_pred: 预测值
+    :return: R^2的值
+    """
+    # 总平方和（Total Sum of Squares, SSE）
+    ss_total = tf.reduce_sum(tf.square(y_true - tf.reduce_mean(y_true)))
+    # 回归平方和（Residual Sum of Squares, SSR）
+    ss_res = tf.reduce_sum(tf.square(y_true - y_pred))
+    # R^2 计算
+    r2 = 1 - ss_res/ss_total
+    return r2
+
+
+def conv_layer(inputs, filters, kernel_size=3, strides=1, need_activate=True):
+    out = keras.layers.Conv2D(filters, kernel_size, strides, padding='same')(inputs)
+    out = keras.layers.BatchNormalization()(out)
+    if need_activate:
+        out = keras.layers.ELU()(out)
+    return out
+
+
+def block_1(inputs, filters):
+    out = conv_layer(inputs, filters // 4, 1)
+    out = conv_layer(out, filters // 4, 3)
+    out = conv_layer(out, filters // 4, 3, need_activate=False)
+    res = conv_layer(inputs, filters // 4, 1)
+    out = keras.layers.ELU()(out + res)
+    return out
+
+
+def block_2(inputs, filters):
+    out = conv_layer(inputs, filters // 4, 1)
+    out = conv_layer(out, filters // 4, 3)
+    out = conv_layer(out, filters // 4, 3, need_activate=False)
+    res = conv_layer(inputs, filters // 4, 1)
+    out = keras.layers.ELU()(out + res)
+    return out
+
+
+def block_3(inputs, filters):
+    out = conv_layer(inputs, filters // 4, 1)
+    out = conv_layer(out, filters // 4, 3)
+    out = conv_layer(out, filters, 3, need_activate=False)
+    res = conv_layer(inputs, filters, 1)
+    out = keras.layers.ELU()(out + res)
+    return out
+
+
+def dr_unet(pretrained_weights=None, input_size=(128, 128, 1), dims=32):
+    inputs = keras.Input(input_size)
+    out = conv_layer(inputs, 16, 1)
+
+    out = block_1(out, dims)
+    out_256 = block_3(out, dims)
+    out = keras.layers.MaxPool2D(2, 2, padding='same')(out_256)
+
+    out = block_1(out, dims * 2)
+    out_128 = block_3(out, dims * 2)
+    out = keras.layers.MaxPool2D(2, 2, padding='same')(out_128)
+
+    out = block_1(out, dims * 4)
+    out_64 = block_3(out, dims * 4)
+    out = keras.layers.MaxPool2D(2, 2, padding='same')(out_64)
+
+    out = block_1(out, dims * 8)
+    out_32 = block_3(out, dims * 8)
+    out = keras.layers.MaxPool2D(2, 2, padding='same')(out_32)
+
+    out = block_1(out, dims * 16)
+    out_16 = block_3(out, dims * 16)
+    out = keras.layers.MaxPool2D(2, 2, padding='same')(out_16)
+
+    out = block_1(out, dims * 32)
+    out = block_3(out, dims * 32)
+
+    up_16 = keras.layers.Conv2DTranspose(filters=dims * 16, kernel_size=2, strides=2, padding='same')(out)
+    up = keras.layers.Concatenate()([up_16, out_16])
+    up = keras.layers.BatchNormalization()(up)
+    up = keras.layers.ELU()(up)
+
+    up = block_2(up, dims * 16)
+    up = block_3(up, dims * 16)
+    up_32 = keras.layers.Conv2DTranspose(filters=dims * 8, kernel_size=2, strides=2, padding='same')(up)
+    up = keras.layers.Concatenate()([up_32, out_32])
+    up = keras.layers.BatchNormalization()(up)
+    up = keras.layers.ELU()(up)
+
+    up = block_2(up, dims * 8)
+    up = block_3(up, dims * 8)
+    up_64 = keras.layers.Conv2DTranspose(filters=dims * 4, kernel_size=2, strides=2, padding='same')(up)
+    up = keras.layers.Concatenate()([up_64, out_64])
+    up = keras.layers.BatchNormalization()(up)
+    up = keras.layers.ELU()(up)
+
+    up = block_2(up, dims * 4)
+    up = block_3(up, dims * 4)
+    up_128 = keras.layers.Conv2DTranspose(filters=dims * 2, kernel_size=2, strides=2, padding='same')(up)
+    up = keras.layers.Concatenate()([up_128, out_128])
+    up = keras.layers.BatchNormalization()(up)
+    up = keras.layers.ELU()(up)
+
+    up = block_2(up, dims * 2)
+    up = block_3(up, dims * 2)
+    up_256 = keras.layers.Conv2DTranspose(filters=dims * 1, kernel_size=2, strides=2, padding='same')(up)
+    up = keras.layers.Concatenate()([up_256, out_256])
+    up = keras.layers.BatchNormalization()(up)
+    up = keras.layers.ELU()(up)
+
+    up = block_2(up, dims)
+    up = block_3(up, dims)
+    up = keras.layers.Conv2D(filters=1, kernel_size=1, strides=(1, 1), padding='same')(up)
+    up = keras.activations.sigmoid(up)
+
+    model = keras.Model(inputs, up)
+
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-5),
+                  loss=bce_dice_loss,
+                  metrics=['accuracy', bce_dice_loss, dice_loss, iou_coefficient, r_squared])
+
+    if pretrained_weights is not None:
+        model.load_weights(pretrained_weights)
+        print("\nmodel load successfully!\n")
+    return model
+
+
+if __name__ == '__main__':
+    import numpy as np
+
+
+    def generate_fake_data(num_samples, input_size):
+        # 生成随机数据作为样本
+        x = np.random.random((num_samples,) + input_size)
+        # 生成随机二进制标签作为样本标签
+        y = np.random.randint(0, 2, (num_samples,) + input_size)
+        return x, y
+
+
+    num_samples = 10
+    input_size = (128, 128, 1)
+    x_train, y_train = generate_fake_data(num_samples, input_size)
+
+    model = dr_unet(pretrained_weights=r"D:\WeChat Files\wxid_s1m1kt6lt2eo22\FileStorage\File\2024-04\DR_UNet_CV0.keras",
+                    input_size=input_size)
+    model.fit(x_train, y_train, epochs=5, batch_size=1)
---
+++ 计算HV.py
@@ -0,0 +1,102 @@
+import sys
+from collections import Counter
+import pathlib
+import math
+import os
+
+import matplotlib.pyplot as plt
+# import tensorflow as tf
+import numpy as np
+import cv2 as cv
+import tqdm
+from PIL import Image
+
+"""
+
+这里的代码通过模型输出的分割结果，进行血肿体积计算。
+
+
+"""
+
+
+
+def calculate_hemorrhage_volume_from_fullCT(label_dir, pixel_spacing=0.75, slice_thickness=5):
+    """
+    计算基于CT标签图像的血肿体积。
+
+    :param image_dir: 存放CT图像的目录路径
+    :param label_dir: 存放CT标签图像的目录路径
+    :param pixel_spacing: CT图像中每个像素代表的实际距离（毫米）
+    :param slice_thickness: 每张CT切片的厚度（毫米）
+    :return: 返回一个字典，键为病人ID，值为相应的预测血肿体积（毫升）
+    """
+    # 每个体素的体积
+    voxel_volume = pixel_spacing * pixel_spacing * slice_thickness
+
+    # 用来存储每个病人血肿体积的字典
+    hemorrhage_volumes = {}
+
+    # 读取所有标签文件名
+    label_files = os.listdir(label_dir)
+
+    for label_file in label_files:
+        # 构建完整的文件路径
+        label_path = os.path.join(label_dir, label_file)
+
+        # 加载标注图像
+        label_image = Image.open(label_path)
+        label_array = np.array(label_image)
+
+        # 计算白色像素的数量（即肿块的体积）
+        hemorrhage_volume = np.sum(label_array >= 254) * voxel_volume
+
+        # 解析病人ID和切片编号
+        patient_id, _ = label_file.split('.')[0].split('_')
+
+        # 将体积加到该病人的总体积中
+        hemorrhage_volumes[patient_id] = hemorrhage_volumes.get(patient_id, 0) + hemorrhage_volume
+
+    # 返回每个病人的血肿总体积（转换为毫升）
+    return {patient_id: volume / 1000 for patient_id, volume in hemorrhage_volumes.items()}
+
+
+def calculate_hemorrhage_volume_from_subCT(path, pixel_spacing=0.75, slice_thickness=5.0):
+    # 用于存储每个病人血肿体积的字典
+    hemorrhage_volumes = {}
+
+    for filename in os.listdir(path):
+        if filename.endswith(".png"):
+            # 解析文件名以获取病人ID
+            patient_id = filename.split('_')[0]
+            # 读取图像文件
+            img = Image.open(os.path.join(path, filename))
+            # 将图像转换为灰度数组
+            img_array = np.array(img)
+            # 计算标记为血肿的白色像素点的数量
+            white_pixels = np.sum(img_array > 0)
+            # 调整像素点数量以匹配原始CT图像的尺寸，计算血肿体积
+            vol = white_pixels * slice_thickness * pixel_spacing
+            # 将计算得出的体积加到对应病人ID的总体积中
+            hemorrhage_volumes[patient_id] = hemorrhage_volumes.get(patient_id, 0) + vol
+
+    # 返回每个病人的预测血肿体积结果
+    return {patient_id: volume / 1000 for patient_id, volume in hemorrhage_volumes.items()}
+
+
+if __name__ == '__main__':
+
+    ##################--下面的是通过完整的CT标注图计算血肿体积--##################
+
+    # 使用示例
+    label_dir = r"C:\Users\1\Downloads\DR_Unet_on_kaggle\DataV1\CV0\test\fullCT\label"
+    # 使用完整的CT图进行计算血肿体积
+    volumes = calculate_hemorrhage_volume_from_fullCT(label_dir)
+
+    # 打印每个病人的血肿总体积
+
+    print('完整CT图的血肿体积计算结果\n'
+          '键值对为：\n'
+          'Patient : hemorrhage volume(ml)')
+    print(sorted(volumes.items(), key=lambda x: x[0]))
+
+
